2d6eafee5aad7d537ff85e4d3bccec6be17807c7
Hossein <hosseinrouhi79@gmail.com>
2023-05-19 17:42:50 +0330

    print the form data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -19,4 +20,28 @@ func main() {
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmp := template.Must(template.ParseFiles("index.html"))
 	tmp.Execute(w, nil)
+}
+
+func submitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	name := r.Form.Get("name")
+	email := r.Form.Get("email")
+
+	formData := FormData{
+		Name:  name,
+		Email: email,
+	}
+
+	fmt.Printf("Name: %v , Email: %v", formData.Name, formData.Email)
+
 }
